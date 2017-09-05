961aa5f74cf0690613b03d85d15da4517178af70
DA96 <31645482+DA96@users.noreply.github.com>
2017-09-05 06:41:17 +0000

    1

---
+++ hello world.py
@@ -0,0 +1,13 @@
+def output() :
+
+    ''' objective : to print hello world'''
+    print ('Hello World') 
+
+def main():
+    '''calling output function'''
+    output()
+
+
+if __name__=='__main__':
+    '''calling main'''
+    main()
